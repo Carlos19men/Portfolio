f50b23c2f07b8a94af0ea49be6ce5798dd128437
carlos mendez <carlosalcides190304@gmail.com>
2025-08-10 22:11:54 -0400

    porfin solucioné

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import icon from "astro-icon";
 import tailwindcss from '@tailwindcss/vite';
 
-import icon from 'astro-icon';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  integrations: [icon()],
 
-  integrations: [tailwindcss(),icon()],
+  vite: {
+    plugins: [tailwindcss(),icon()],
+  },
 });
